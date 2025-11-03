022c7a693db24dc2952e086231bab355e19e6ee8
joelchaconcastillo <chaconcastillojoel@gmail.com>
2025-11-03 17:33:46 -0600

    0

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
+#from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List, Optional
 from uuid import uuid4
